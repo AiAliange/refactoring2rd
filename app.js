6d6c016980c66fd7ad6eaad9e2bce32a024f991c
AiAliange <hongliangdong@163.com>
2020-04-16 17:03:44 +0800

    p7 提炼函数

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-console.log("hello world!");
-console.log("good night!");
+var ch01 = require("./ch01/statement")
+var invoices = require("./ch01/invoices.json");
+var plays = require("./ch01/plays.json");
+
+console.log("hello world!\n");
+
+var rtn =ch01.statement(invoices[0],plays);
+console.log(rtn);
+ 
+console.log("good night!\n");
