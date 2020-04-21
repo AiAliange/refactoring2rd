6dda60ffe2583074d8828435bad4a8c17cb924e5
AiAliange <hongliangdong@163.com>
2020-04-21 15:43:55 +0800

    P22 end

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var ch01 = require("./ch01/statement")
 var invoices = require("./ch01/invoices.json");
-var plays = require("./ch01/plays.json");
 
 console.log("hello world!\n");
 
-var rtn =ch01.statement(invoices[0],plays);
+var rtn =ch01.statement(invoices[0]);
 console.log(rtn);
  
 console.log("good night!\n");
--- ch01/statement.js
+++ ch01/statement.js
@@ -1,49 +1,92 @@
-function statement(invoice, plays) {
-    let totalAmount = 0;
-    let volumeCredits = 0;
+var plays = require("./plays.json");//剧目
+
+function statement(invoice) {
     let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 2
-    }).format;
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
-        let thisAmount = amountFor(perf,play);
-        // add volume credits
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("commdy" == play.type) {
-            volumeCredits += Math.floor(perf.audience / 5);
-        }
-        // print line for this order
-        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
+    result += `Amount owed is ${usd(totalAmount(invoice))}\n`;
+    result += `You earned ${totalVolumeCredits(invoice)} credits\n`;
+    return result;
+}
+/**
+ * 客户订单总费用
+ * @param {object} invoice 客户演出订单
+ */
+function totalAmount(invoice){
+    let result = 0;
+    for(let perf of invoice.performances){
+        result += amountFor(perf);
+    }
     return result;
 }
 
-function amountFor(perf, play){
+/**
+ * 计算一场演出的费用
+ * @param {object} aPerformance 一场演出
+ */
+function amountFor(aPerformance){
     let result = 0;
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
         case "tragedy":
             result = 40000;
-            if (perf.audience > 30) {
-                result += 1000 * (perf.audience - 30);
+            if (aPerformance.audience > 30) {
+                result += 1000 * (aPerformance.audience - 30);
             }
             break;
         case "comedy":
             result = 30000;
-            if (perf.audience > 20) {
-                result += 10000 + 500 * (perf.audience - 20);
+            if (aPerformance.audience > 20) {
+                result += 10000 + 500 * (aPerformance.audience - 20);
             }
-            result += 300 * perf.audience;
+            result += 300 * aPerformance.audience;
             break;
         default:
-            throw new Error(`Unkown type:${play.type}\n`);
+            throw new Error(`Unkown type:${playFor(aPerformance).type}\n`);
     }
     return result;
+}
+
+/**
+ * 客户订单总积分
+ * @param {object} invoice 客户演出订单
+ */
+function totalVolumeCredits(invoice){
+    let result = 0;
+    for (let perf of invoice.performances) {
+        result += volumeCreditsFor(perf);
+    }
+    return result;
+}
+
+/**
+ * 单场演出观众量积分
+ * @param {object} aPerformance 单场演出
+ */
+function volumeCreditsFor(aPerformance){
+    let result = 0;
+     // add volume credits
+     result += Math.max(aPerformance.audience - 30, 0);
+    // add extra credit for every ten comedy attendees
+    if ("commdy" == playFor(aPerformance).type) {
+        result += Math.floor(aPerformance.audience / 5);
+    }
+    return result;
+}
+/**
+ * 格式化美元
+ * @param {Number} aNumber 小钱钱
+ */
+function usd(aNumber){
+    return new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2
+    }).format(aNumber/100);
+}
+
+function playFor(aPerformance){
+    return plays[aPerformance.playID];
 }
 
 exports.statement = statement;
